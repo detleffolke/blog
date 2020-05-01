42821356d3b7675640a517ee18bc3b4022a02f1e
detleffolke <detlef.folke@gmail.com>
2020-05-01 15:50:56 +0200

    modifikationen

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
+
 const app = express();
+app.use(bodyParser.json());
+
 const posts = {}
 
 app.get('/posts', (req, res)  => {
